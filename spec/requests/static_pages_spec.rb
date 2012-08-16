a67d3b9cf92db4205303f5842375dd4cc85171d3
p-a-g <post.an.gerrit@arcor.de>
2012-08-16 19:27:05 +0200

    Add a full_title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  let(:base_title) { "TP4943 Demo Application |" }
+
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Demo Application')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "TP4943 Demo Application")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1',
+                                :text => "Help")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "#{base_title} Help")
+    end
+    
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1',
+                                :text => "About Us")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "#{base_title} About")
+    end
+    
+  end
+  
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1',
+                                :text => "Contact")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text => "#{base_title} Contact")
+    end
+    
+  end
+  
+  
+end
